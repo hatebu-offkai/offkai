34f9301a152eb1dec4a817dfeb8deb83f63e857c
yuiseki <yuiseki@gmail.com>
2014-07-08 14:44:06 +0900

    update mouse.js

--- static/js/mouse.js
+++ static/js/mouse.js
@@ -1,15 +1,17 @@
-
 function mouseCustom() {
-    var src = "/static/img/profile_s.gif"
-    var ganmen = $("<div class='ganmen'><img src='"+src+"'></div>")
+    var src = "/static/img/profile_s.gif";
+    var ganmen = $("<div class='ganmen'><img src='"+src+"'></div>");
     $(ganmen).css({overflow:'hidden', position:'absolute', top:'0', left:'0'});
     $('body').append(ganmen);
-    $('html').mousemove(function(e){
-        var icon = $(ganmen).offset();
-        $(ganmen).stop();
-        $(ganmen).animate({
-            top:e.pageY+50,
-            left:e.pageX+50
+
+    $('html').mousemove(function(ev){
+        var position = [10, 25, 40, 50, 70];
+        var randPosX = position[Math.floor(Math.random()*position.length)];
+        var randPosY = position[Math.floor(Math.random()*position.length)];
+        $(".ganmen").stop();
+        $(".ganmen").animate({
+            top:ev.pageY+50,
+            left:ev.pageX+50
             },{
                 duration: 1000,
                 specialEasing:{
