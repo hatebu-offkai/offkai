0d0b0bf8324dc7c33130ec3115759cdc7888bd5a
yuiseki <yuiseki@gmail.com>
2014-07-10 16:05:45 +0900

    more saito following mouse

--- static/js/mouse.js
+++ static/js/mouse.js
@@ -1,22 +1,32 @@
 function mouseCustom() {
-    var src = "/static/img/profile_s.gif";
-    var ganmen = $("<div class='ganmen'><img src='"+src+"'></div>");
-    $(ganmen).css({overflow:'hidden', position:'absolute', top:'0', left:'0'});
-    $('body').append(ganmen);
+    var src = "/static/img/profile.gif";
+    var createGanmen = function(){
+      var ganmen = $("<div class='ganmen'><img src='"+src+"' style='width:20px;height:20px;'></div>");
+      $(ganmen).css({overflow:'hidden', position:'absolute', top:'0', left:'0'});
+      $('body').append(ganmen);
+      return ganmen;
+    };
+    var ganmens = [];
+    _(30).times(function(n){
+        ganmens.push(createGanmen());
+    });
 
     $('html').mousemove(function(ev){
-        var position = [10, 25, 40, 50, 70];
-        var randPosX = position[Math.floor(Math.random()*position.length)];
-        var randPosY = position[Math.floor(Math.random()*position.length)];
-        $(".ganmen").stop();
-        $(".ganmen").animate({
-            top:ev.pageY+50,
-            left:ev.pageX+50
+        var position = [-50, -40, -30, -20, 20, 30, 40, 50];
+        _.each(ganmens, function(ganmen){
+            var randPosX = position[Math.floor(Math.random()*position.length)];
+            var randPosY = position[Math.floor(Math.random()*position.length)];
+            $(ganmen).stop();
+            $(ganmen).animate({
+                top:ev.pageY+randPosY,
+                left:ev.pageX+randPosX
             },{
                 duration: 1000,
                 specialEasing:{
                     top:'easeOutCirc',
                     left:'easeOutCirc'
-                }});
+                }
+            });
+        });
     });
 }
