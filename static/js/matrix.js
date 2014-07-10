cfec462a197044565643468099b7f11d31a44f09
yuiseki <yuiseki@gmail.com>
2014-07-10 19:44:34 +0900

    matrix 齊藤貴義

--- static/js/matrix.js
+++ static/js/matrix.js
@@ -4,12 +4,15 @@ var width = q.width = s.width;
 var height = q.height = s.height;
 var letters = Array(256).join(1).split('');
 
+
 var draw = function () {
   q.getContext('2d').fillStyle='rgba(0,0,0,.05)';
   q.getContext('2d').fillRect(0,0,width,height);
   q.getContext('2d').fillStyle='#0F0';
+  var textBase = "齊藤貴義".split("");
   letters.map(function(y_pos, index){
-    text = String.fromCharCode(3e4+Math.random()*33);
+    //text = String.fromCharCode(3e4+Math.random()*33);
+    text = textBase[Math.floor(Math.random()*textBase.length)];
     x_pos = index * 10;
     q.getContext('2d').fillText(text, x_pos, y_pos);
     letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
